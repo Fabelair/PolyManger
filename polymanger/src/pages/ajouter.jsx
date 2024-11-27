31d516061d084320ec6189b108a06677444a4d81
fabelair <Belair.fa@outlook.com>
2024-11-27 00:43:10 -0500

    nav ajouter page

---
+++ polymanger/src/pages/ajouter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Ajouter(){
+    return <div class="h-screen">
+        <div class="content-center grid grid-rows-2">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">@PolyManger</span>
+            <div></div>
+        </div>
+    </div>
+}
+
+export default Ajouter;
--- polymanger/src/pages/home.jsx
+++ polymanger/src/pages/home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Home(){
-    return <h1>home!</h1>
+    return <div class="h-screen">
+
+    </div>
 }
 
 export default Home;
