5a069fd3b28dfcaba7889e5609a5e003580c464e
fabelair <Belair.fa@outlook.com>
2024-11-26 23:40:06 -0500

    navigation

--- polymanger/src/App.jsx
+++ polymanger/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";//npm i -D react-router-dom@latest
+import './index.css';
+import Footer from './composants/footer';
+import Navbar from './composants/navbar';
+import Home from './pages/home';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ul class="select-none">
+      <div class="min-h-100vh flex flex-col">
+        <BrowserRouter>
+        <Navbar/>
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+        </Routes>
+        </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Footer/>
+    </ul>
+
   )
 }
 
---
+++ polymanger/src/composants/navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar(){
+    return <h1>Je suis la navbar</h1>
+}
+
+export default Navbar;
