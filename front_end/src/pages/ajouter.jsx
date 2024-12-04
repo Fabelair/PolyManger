69568e8ffa144ed55df2c54e2478ec313903cd9b
fabelair <Belair.fa@outlook.com>
2024-12-04 16:47:51 -0500

    ajouter page ui

--- front_end/src/pages/ajouter.jsx
+++ front_end/src/pages/ajouter.jsx
@@ -3,39 +3,77 @@ import { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form'; //npm install react-hook-form
 function Ajouter(){
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const [recetteState, setRecetteState] = useState(false);
     const onSubmit = async (data) => {
         console.log("Submit")
       };
+    const ingredient = () =>{
+      event.preventDefault()
+      console.log("ingredient ++")
+    }
     return (
-    <div class="w-[100%] h-screen">
+    <div class="w-[100%] h-flex mb-16">
       <div class="w-[100%] h-flex">
-        <div class="mt-32 h-flex">
-          <div class="w-[32rem] h-flex rounded-2xl shadow-xl ring ring-slate-700 place-self-center grid grid-cols-2">
-              <div class="w-flex h-16 ring"></div>
+        <h1 class="dark:text-white place-self-center mt-24 font-bold text-4xl">Ajouter votre touche!</h1>
+        <div class="mt-16 h-flex">
+          <div class="w-[32rem] h-flex rounded-2xl shadow-xl  place-self-center grid grid-cols-2">
+              <div class="w-flex h-16 p-2 ">
+                {recetteState && <div class="place-self-center h-full w-52 rounded-2xl shadow-xl ring ring-blue-700 grid">
+                  <div class="mt-3"><h1 class="dark:text-white text-center">Recette</h1></div>
+                </div>}
+                {!recetteState && <div class="place-self-center h-full w-52 rounded-2xl shadow-xl grid" onClick={() =>setRecetteState(true)}>
+                  <div class="mt-3"><h1 class="dark:text-white text-center">Recette</h1></div>
+                </div>}
+              </div>
+              <div class="w-flex h-16 p-2 ">
+                {!recetteState && <div class="place-self-center h-full w-52 rounded-2xl shadow-xl ring ring-blue-700 grid">
+                  <div class="mt-3">
+                    <h1 class="dark:text-white text-center">Plan Alimentaire</h1>
+                  </div>
+                </div>}
+                {recetteState && <div class="place-self-center h-full w-52 rounded-2xl shadow-xl grid" onClick={()=>setRecetteState(false)}>
+                  <div class="mt-3">
+                    <h1 class="dark:text-white text-center">Plan Alimentaire</h1>
+                  </div>
+                </div>}
+              </div>
           </div>
         </div>
-        <div class="w-[32rem] h-flex rounded-2xl shadow-xl ring ring-slate-700 place-self-center">
+        <div class="w-[32rem] h-flex rounded-2xl mt-4 shadow-xl ring ring-slate-700 place-self-center grid">
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div class="w-[80%] ml-[10%] mt-4">
-              <label for="message" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Full Name</label>
-              <input type="text" id="UserName" {...register("name", { required: true })} class=" rounded-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name"></input>
-              {errors.name && <p class="text-red-500">Your name is required</p>}
+            <div class="w-[80%] ml-[10%] mt-4 h-24 ">
+              <label for="message" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Nom :</label>
+              <input type="text" id="nom" {...register("name", { required: true })} class=" rounded-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nom"></input>
+              {errors.name && <p class="text-red-500">Tu n'as pas mis de nom à cette recette</p>}
             </div>
-            <div class="w-[80%] ml-[10%] mt-4">
-              <label for="message" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Email</label>
-              <input type="text" id="Email" {...register("email", { required: true, pattern: /^\S+@\S+$/i })}  class=" rounded-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email@gmail.com"></input>
-              {errors.email && <p class="text-red-500">Email is required</p>}
+            <div class="w-[80%] ml-[10%] h-24 ">
+              <label for="message" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Prix par portion ($) :</label>
+              <input type="text"  {...register("name", { required: true })} class=" rounded-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Prix"></input>
+              {errors.name && <p class="text-red-500">Les étudiants seraient ravis de connaitre le prix/portion!</p>}
             </div>
-            <div class="w-[80%] ml-[10%] mt-4">
-              <label for="message" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Spotify URL</label>
-              <input {...register("spotifyUrl", { required: true })} type="text" id="SpotifyURL" class=" rounded-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="e.g. https://open.spotify.com/artist/hash"></input>
-              {errors.spotifyUrl && <p class="text-red-500">Spotify Url is required</p>}
+            <div class="w-[80%] ml-[10%] h-24 ">
+              <label for="message" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Portions :</label>
+              <input type="text" {...register("name", { required: true })} class=" rounded-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="eg. 4"></input>
+              {errors.name && <p class="text-red-500">Combien de portions?</p>}
             </div>
-            <label for="message" class=" mt-4 ml-[10%] block mb-2 text-md font-medium text-gray-900 dark:text-white">Wallet</label>
-            <div class="w-[80%] ml-[10%] mt-10">
-              <div class="w-full grid">
-                <button type="submit" class="mb-4 place-self-center w-32 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit</button>
+            <div class="w-[80%] ml-[10%] h-24 ">
+              <label for="message" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Temps de cuisson :</label>
+              <input type="text" {...register("name", { required: true })} class=" rounded-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="eg. 1h30"></input>
+              {errors.name && <p class="text-red-500">Quel est le temps de cuisson?</p>}
+            </div>
+            <div class="w-[80%] ml-[10%] h-24 ">
+              <label for="message" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Temps de préparation :</label>
+              <input type="text"  {...register("name", { required: true })} class=" rounded-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="eg. 30min"></input>
+              {errors.name && <p class="text-red-500">Quel est le temps de préparation?</p>}
+            </div>
+            <div class="w-[80%] ml-[10%] h-flex mb-4">
+              <label for="message" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Ingrédients :</label>
+              <div class=" h-flex grid grid-cols-2">
+                <label for="message" class="block mb-2 ml-2 text-md font-medium text-gray-900 dark:text-white">Nom</label>
+                <label for="message" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Quantité</label>
               </div>
+              <button onClick={()=>{ingredient()}} class=" rounded-lg bg-gray-50 border border-gray-300 focus:ring-gray-500 focus:border-gray-500  w-full place-self-center text-4xl text-center  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-white">+</button>
+              {errors.name && <p class="text-red-500">Tu n'as pas mis d'ingrédients</p>}
             </div>
           </form>
         </div>
