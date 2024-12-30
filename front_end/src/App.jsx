8e10804be62d565e3f217dd88e63b98c11135bfd
fabelair <Belair.fa@outlook.com>
2024-12-30 16:51:45 -0500

    github routing now working

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";//npm i -D react-router-dom@latest
+import { HashRouter as Router, Routes, Route } from "react-router-dom";//npm i -D react-router-dom@latest
 import './index.css';
 import Footer from './composants/footer';
 import Navbar from './composants/navbar';
 import Home from './pages/home';
 import Ajouter from './pages/ajouter';
+import Connect from './pages/connect';
 
 function App() {
 
   return (
     <ul class="select-none dark:bg-gray-900">
       <div class="min-h-100vh flex flex-col">
-        <BrowserRouter>
+        <Router>
         <Navbar/>
         <Routes>
-            <Route path="/" element={<Home/>}></Route>
             <Route path="/Ajouter" element={<Ajouter/>}></Route>
+            <Route path="/connect" element={<Connect/>}></Route>
+            <Route path="/" element={<Home/>}></Route>
         </Routes>
-        </BrowserRouter>
+        </Router>
       </div>
       <Footer/>
     </ul>
---
+++ front_end/src/pages/connect.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Connect(){
+    return (<div class="h-screen grid">
+        <div class="place-self-center ring h-[40rem] w-[60rem] grid grid-cols-2">
+            <div class="ring w-full h-flex mt-4 ml-4 mr-8 mb-4 rounded-xl"></div>
+            <div class="ring w-flex h-flex mt-4 ml-8 mr-4 mb-4 rounded-xl"></div>
+        </div>
+    </div>)
+}
+
+export default Connect;
